efb68f8f0809100d999b0c71d8739141576cd8fc
Melam Sanjana Chakradhar <24pa1a12d7@vishnu.edu.in>
2026-08-04 00:24:59 +0530

    change

--- backend/src/main/java/com/codelens/backend/BackendApplication.java
+++ backend/src/main/java/com/codelens/backend/BackendApplication.java
@@ -3,11 +3,15 @@ package com.codelens.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
+    }
+)
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
