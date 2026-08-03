2089b0ac8bb5c709b23175fbd588b8902af49ff7
Melam Sanjana Chakradhar <24pa1a12d7@vishnu.edu.in>
2026-08-04 00:43:44 +0530

    connection

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://codelens-ai-qqf9.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
